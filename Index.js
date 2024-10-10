a1a20b3fa889198bc94764b2288e6235f0ef4fd1
Rebecca-ADS <rebeccaimoveisararaquara@gmail.com>
2024-10-09 21:36:13 -0300

    Add files via upload

---
+++ Index.js
@@ -0,0 +1,9 @@
+// npm install readline-sync
+const readline = require("readline-sync");
+
+const sexo = "F";
+
+if (sexo == "M") {
+    console.log("Pode entrar no banheiro masculino.");
+} else {
+    console.log("Não pode entrar no banheiro masculin.");
